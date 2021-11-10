9fccead0d6365a0da66cd63b0ef430fe15e2c533
Gilbertoaleite <gilbertoaleite@hotmail.com>
2021-11-10 17:13:06 -0300

    corrigindo bug

--- src/data/services/ApiService.ts
+++ src/data/services/ApiService.ts
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const url = 'https://ediaristas-workshop.herokuapp.com'; //"http://127.0.0.1:8000/";
+const url = "http://127.0.0.1:8000/";
 //"https://ediaristas-workshop.herokuapp.com"; api teste
 
 export const ApiService = axios.create({
